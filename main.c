3b3ed1a42bbf469571a093e81bdde05fc638beea
ararra <ali.alwan0@gmail.com>
2025-05-27 20:29:35 +0200

    initialize SDL and made structs

--- main.c
+++ main.c
@@ -1,5 +1,58 @@
 #include "main.h"
 
-int main(int argc, char *argv[])
+#include <SDL3/SDL.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(int argc, char* argv[]) {
+    initialize_game();
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(g_game.renderer, 0, 0, 0, 255); // Black background
+        SDL_RenderClear(g_game.renderer);
+
+        // Draw something here...
+
+        SDL_RenderPresent(g_game.renderer);
+    }
+
+    SDL_DestroyRenderer(g_game.renderer);
+    SDL_DestroyWindow(g_game.window);
+    SDL_Quit();
+
+    return 0;
+}
+
+
+void initialize_game()
 {
+    if (SDL_Init(SDL_INIT_VIDEO ) != 1) {
+        SDL_Log("Unable to initialize SDL video: %s", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    g_game.window = SDL_CreateWindow("SDL3 Boilerplate", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (!g_game.window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    g_game.renderer = SDL_CreateRenderer(g_game.window, NULL);
+    if (!g_game.renderer) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(g_game.window);
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
 }
--- main.h
+++ main.h
@@ -1,2 +1,72 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+///Defining structs
+typedef struct Game
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+
+
+}Game;
+
+
+typedef struct Weapon
+{
+    int damage;
+    int max_ammo;
+    int ammo;
+
+}Weapon;
+
+typedef struct Character
+{
+    SDL_Rect position;
+    SDL_Texture* texture;
+
+    int max_health;
+    int health;
+    
+    int armor;
+
+    int speed;
+
+    int max_gas;
+    int gas;
+
+    int bomb_capacity;
+    int amount_bombs;
+
+    // from lower to upper:
+    // coal, iron, copper, gold, emerald, diamond, mythril, titanium
+    int bag_capacity;
+    int ores[8];
+
+    Weapon gun;
+
+
+
+}Char;
+
+
+
+
+typedef struct Enemies
+{
+    SDL_Rect position[5];
+    SDL_Texture* texture[5];
+    // This needs to be able to consider different types of enemies, was thinking union but will look at later.
+}Enemies;
+
+//declaring global variables
+Game g_game;
+Char g_char;
+
+
+
+
+void initialize_game();

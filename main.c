e8edfefe1de73eca0aee6749e7cedfd0a9a25ff4
ararra <ali.alwan0@gmail.com>
2025-06-10 17:42:06 +0200

    fix collisions using vector based method

--- main.c
+++ main.c
@@ -27,23 +27,27 @@ int main(int argc, char* argv[]) {
         delta = (now-last)/1000.0f;
 
         const bool *keys = SDL_GetKeyboardState(NULL);
-        SDL_FRect result;
-        SDL_FRect saved_result[3];
+        SDL_FRect temp_collision_result;
+        SDL_FRect collision_result_array[4];
         int k = 0;
-        // TODO: Generalize downward collision into all directions
         for(int i = 0; i < AMOUNTOFTILESX*AMOUNTOFTILESY; i++)
         {
-            if (SDL_GetRectIntersectionFloat(&g_char.position, &g_game.tile_position_array[i], &result) & !collision)
+            if (SDL_GetRectIntersectionFloat(&g_char.position, &g_game.tile_position_array[i], &temp_collision_result))
             {
-                saved_result[k] =  result;
+                collision_result_array[k] = temp_collision_result;
                 k += 1;
-                SDL_Log("coll ");
-                collision = true;
-                collision_start = now;
-                tile_col_index = i;
+                SDL_Log("coll %d ", k);
+
+                if(!collision)
+                {
+                    collision_start = now;
+                }
                 
+                collision = true;
+                tile_col_index = i;
             }
         }
+        
         
         // TODO: add slowdown to movement when letting go of button
         if (keys[SDL_SCANCODE_W] & !collision){ g_char.position.y -= delta*g_char.speed; }
@@ -51,54 +55,61 @@ int main(int argc, char* argv[]) {
         if (keys[SDL_SCANCODE_A] & !collision){ g_char.position.x -= delta*g_char.speed; }
         if (keys[SDL_SCANCODE_D] & !collision){ g_char.position.x += delta*g_char.speed; }
 
-        // issues with current collisions:
-        // 1) teleports drill to the top when side collision
-        // 2) 
 
-        //pushes char back if collision and the button has been let go.
         //TODO: Make bounceback feature initial velocity based on "pressure" (time under button press) with exponential speed loss. 
         //Needs to be done between frames right so a function with static variable right?
-        //this is why the side collision jump is happening
-        //dont know if i should implement drilling upwards or leave it as it is
-        // if (collision & (!keys[SDL_SCANCODE_S] | (!keys[SDL_SCANCODE_S] & (!keys[SDL_SCANCODE_A] | !keys[SDL_SCANCODE_D]) ))  )
-        // {   
-        //     //the constant needs to be there otherwise the colision persists for a few frames and it activates the tile collision if check above several times.
-        //     // use the result rect from the collision to create a direction for the bounce back.
-        //     g_char.position.y -= 5+delta*g_char.speed;
-        //     collision = false;
-        // }         
+
+
         if (collision && (!(keys[SDL_SCANCODE_S] || (keys[SDL_SCANCODE_A] ^ keys[SDL_SCANCODE_D])))  )
         {   
             //the constant needs to be there otherwise the colision persists for a few frames and it activates the tile collision if check above several times.
             // use the saved_result rect from the collision to create a direction for the bounce back.
 
             // this method not quite working
-            float saved_result_x_sum = saved_result[0].x +saved_result[1].x +saved_result[2].x;
-            float saved_result_y_sum = saved_result[0].y +saved_result[1].y +saved_result[2].y;
-            int power_y = (saved_result_x_sum > g_char.position.y);
-            int power_x = (saved_result_y_sum > g_char.position.x); 
-            g_char.position.y =g_char.position.y + pow(-1,power_y)*(5+saved_result[0].h);
-            g_char.position.x =g_char.position.x + pow(-1,power_x)*(5+saved_result[0].w);
-            SDL_Log("px: %d po: %d w: %f",power_x, pow(-1,power_x), saved_result[0].h);
+            // float saved_result_x_sum = saved_result[0].x +saved_result[1].x +saved_result[2].x;
+            // float saved_result_y_sum = saved_result[0].y +saved_result[1].y +saved_result[2].y;
+            // int power_y = (saved_result[0].y > g_char.position.y);
+            //int power_x = (saved_result[0].x > g_char.position.x); 
+            // g_char.position.y =g_char.position.y + pow(-1,power_y)*(5+saved_result[0].h);
+            //g_char.position.x =g_char.position.x + pow(-1,power_x)*(5+saved_result[0].w);
+            //SDL_Log("px: %d po: %d w: %f",power_x, pow(-1,power_x), saved_result[0].h);
 
 
+            // vector method
+            // vector from center of char to center of colision
+            int char_middle_x = g_char.position.x + g_char.position.w/2;
+            int char_middle_y = g_char.position.y + g_char.position.h/2; 
+            int coll_middle_x = 0;
+            int coll_middle_y = 0;
 
-            //ChatGPT Method Also not working
-            // int dy = saved_result.y - g_char.position.y;
-            // int dx = saved_result.x - g_char.position.x;
-            // if (dy != 0) {
-            //     g_char.position.y += (dy > 0 ? 1 : -1) * (5 + saved_result.h);
-            // }
-            // if (dx != 0) {
-            //     g_char.position.x += (dx > 0 ? 1 : -1) * (5 + saved_result.w);
-            // }
-            // SDL_Log("dx: %d, dy: %d, result.h: %f, result.w: %f", dx, dy, saved_result.h, saved_result.w);
+            // Calculate center of all collisions
+            for(int i = 0; i < k; i++)
+            {
+                coll_middle_x += collision_result_array[i].x + collision_result_array[i].w/2;
+                coll_middle_y += collision_result_array[i].y + collision_result_array[i].h/2;
+            }
 
-            // Vector method
-
-
+            // Get average center
+            if (k > 0) {
+                coll_middle_x /= k;
+                coll_middle_y /= k;
+                
+                // Calculate push-back vector
+                int v_x = char_middle_x - coll_middle_x;
+                int v_y = char_middle_y - coll_middle_y;
+                
+                SDL_Log("%d", v_x );
+                // Apply push-back (with safety checks)
+                if (v_x != 0) {
+                    g_char.position.x += (v_x > 0) ? 2 : -2;
+                }
+                if (v_y != 0) {
+                    g_char.position.y += (v_y > 0) ? 2 : -2;
+                }
+            }
 
             collision = false;
+
         } 
 
 
@@ -121,7 +132,6 @@ int main(int argc, char* argv[]) {
 
         // Draw something here...
         SDL_RenderTexture(g_game.renderer, g_game.background,NULL, NULL);
-        SDL_RenderTexture(g_game.renderer, g_char.texture,NULL, &g_char.position);
         
         //SDL_RenderTexture(g_game.renderer, g_game.basic_tile,NULL, &g_game.tile_position);
 
@@ -130,6 +140,7 @@ int main(int argc, char* argv[]) {
             SDL_RenderTexture(g_game.renderer, g_game.tile_texture_array[i], NULL, &g_game.tile_position_array[i]);
         }
 
+        SDL_RenderTexture(g_game.renderer, g_char.texture,NULL, &g_char.position);
 
 
         SDL_RenderPresent(g_game.renderer);
